af61ef00f2adb36fb01232ab96edd0a5ac692505
Zaid <zaidajani2007@gmail.com>
2020-12-20 11:44:06 +0530

    final commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,39 @@
-const button = document.getElementById('buttonOne');
-const form = document.getElementById('input');
-const synth = window.speechSynthesis;
+var txtInput = document.getElementById("input");
+var voiceList = document.getElementById("voiceList");
+var btnSpeak = document.getElementById("buttonOne");
+var synth = window.speechSynthesis;
+var voices = [];
 
+PopulateVoices();
+
+if (speechSynthesis !== undefined) {
+  speechSynthesis.onvoiceschanged = PopulateVoices;
+}
+
+btnSpeak.addEventListener("click", () => {
+  var toSpeak = new SpeechSynthesisUtterance(txtInput.value);
+  var selectedVoiceName = voiceList.selectedOptions[0].getAttribute(
+    "data-name"
+  );
+  voices.forEach((voice) => {
+    if (voice.name === selectedVoiceName) {
+      toSpeak.voice = voice;
+    }
+  });
+  synth.speak(toSpeak);
+});
+
+function PopulateVoices() {
+  voices = synth.getVoices();
+  var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
+  voiceList.innerHTML = "";
+  voices.forEach((voice) => {
+    var listItem = document.createElement("option");
+    listItem.textContent = voice.name;
+    listItem.setAttribute("data-lang", voice.lang);
+    listItem.setAttribute("data-name", voice.name);
+    voiceList.appendChild(listItem);
+  });
+
+  voiceList.selectedIndex = selectedIndex;
+}
